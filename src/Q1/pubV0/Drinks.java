ff6c46c79a377a0209556bcabdd00454e9d37617
Alynne <alynnemaj@gmail.com>
2018-09-24 04:08:42 -0300

    questao 1

---
+++ src/Q1/pubV0/Drinks.java
@@ -0,0 +1,20 @@
+package Q1.pubV0;
+
+import java.util.HashMap;
+
+public class Drinks {
+    public  HashMap<String, Integer> listaDrinks = new HashMap<>();
+    public static final String ONE_BEER = "hansa";
+    public static final String ONE_CIDER = "grans";
+    public static final String A_PROPER_CIDER = "strongbow";
+    public static final String GT = "gt";
+    public static final String BACARDI_SPECIAL = "bacardi_special";
+
+   public Drinks(){
+       listaDrinks.put(ONE_BEER, 74);
+       listaDrinks.put(ONE_CIDER, 103);
+       listaDrinks.put(A_PROPER_CIDER, 110);
+       listaDrinks.put(GT, 115);
+       listaDrinks.put(BACARDI_SPECIAL, 170);
+   }
+}
