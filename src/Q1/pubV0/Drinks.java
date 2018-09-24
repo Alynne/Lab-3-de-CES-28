04f7c5a0aec3ab0a63c1502bbf4242f445b839e4
Alynne <alynnemaj@gmail.com>
2018-09-24 19:24:52 -0300

    Continuando a refatoração

--- src/Q1/pubV0/Drinks.java
+++ src/Q1/pubV0/Drinks.java
@@ -3,7 +3,7 @@ package Q1.pubV0;
 import java.util.HashMap;
 
 public class Drinks {
-    public  HashMap<String, Integer> listaDrinks = new HashMap<>();
+    private HashMap<String, Integer> listaDrinks = new HashMap<>();
     public static final String ONE_BEER = "hansa";
     public static final String ONE_CIDER = "grans";
     public static final String A_PROPER_CIDER = "strongbow";
@@ -17,4 +17,13 @@ public class Drinks {
        listaDrinks.put(GT, 115);
        listaDrinks.put(BACARDI_SPECIAL, 170);
    }
+    public int getPrice(String drink){
+       int price;
+       if(listaDrinks.containsKey(drink)){
+            price = listaDrinks.get(drink);
+       }
+       else
+           throw new RuntimeException("No such drink exists");
+       return price;
+    }
 }
--- src/Q1/pubV0/Pub.java
+++ src/Q1/pubV0/Pub.java
@@ -4,17 +4,11 @@ package Q1.pubV0;
 public class Pub {
 
     public int computeCost(String drink, boolean student, int amount) {
-        int price;
         Drinks drinks = new Drinks();
         if (amount > 2 && (drink == Drinks.GT || drink == Drinks.BACARDI_SPECIAL)) {
             throw new RuntimeException("Too many drinks, max 2.");
         }
-        if (drinks.listaDrinks.containsKey(drink)) {
-            price = drinks.listaDrinks.get(drink);
-        }
-        else {
-            throw new RuntimeException("No such drink exists");
-        }
+        int price = drinks.getPrice(drink);
         if (student && (drink == Drinks.ONE_CIDER || drink == Drinks.ONE_BEER || drink == Drinks.A_PROPER_CIDER)) {
             price = price - price/10;
         }
